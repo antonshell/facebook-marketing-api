accf86029bd8c2e738cbd331ca83944b9ebaaec0
Anton Shelyganov <antonshell@yandex.ru>
2017-08-18 15:25:55 +0300

    added instalation guide

--- _config.sample.php
+++ _config.sample.php
@@ -1,3 +1,10 @@
 <?php return [
+    'page_id' => '',
     'account_id' => '',
+
+    'app_id' => '',
+    'app_secret' => '',
+    'access_token' => '',
+    'debug' => true,
+    'curl_enable_ssl' => false,
 ];
